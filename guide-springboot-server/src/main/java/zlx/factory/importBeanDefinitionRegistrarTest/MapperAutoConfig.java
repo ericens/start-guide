b11225d62919fa9bf11dc56ab6cb655eb0f76d05
ericens <zenglx@jpush.cn>
2020-04-21 21:56:34 +0800

    包扫描的原理

--- guide-springboot-server/src/main/java/zlx/factory/importBeanDefinitionRegistrarTest/MapperAutoConfig.java
+++ guide-springboot-server/src/main/java/zlx/factory/importBeanDefinitionRegistrarTest/MapperAutoConfig.java
@@ -1,12 +1,13 @@
 package zlx.factory.importBeanDefinitionRegistrarTest;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import zlx.factory.FactoryProcessor;
 
 /**
- * 1. 多引入：引入 MapperAutoConfiguredMyBatisRegistrar.class,FactoryProcessor.class
- * 2. 单引入: MapperAutoConfiguredMyBatisRegistrar
+ * 1. Import多引入：引入 MapperAutoConfiguredMyBatisRegistrar.class,FactoryProcessor.class
+ * 2. Import单引入: MapperAutoConfiguredMyBatisRegistrar
  *
  * 3. MapperAutoConfiguredMyBatisRegistrar里面有 包扫描，
  *    1. 指定扫描范围
@@ -16,4 +17,29 @@ import zlx.factory.FactoryProcessor;
 //@Import(value = {MapperAutoConfiguredMyBatisRegistrar.class,FactoryProcessor.class})
 @Import(value = {MapperAutoConfiguredMyBatisRegistrar.class})
 public class MapperAutoConfig {
+
+    public MapperAutoConfig() {
+        System.out.println("TestConfig容器初始化...");
+    }
+
+
+    @Bean(name = "getMyCar")
+    public Car getCar() {
+        Car c = new Car();
+        c.setName("dankun");
+        return c;
+    }
+
+    static public class Car {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
 }
